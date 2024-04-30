7d4dc10ec4d72205ab3eac8235a260e97148812e
Danish Anwar <129545709+BinarySapling@users.noreply.github.com>
2024-05-01 00:00:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,185 @@
+// gsap.to("#nav",{
+//     backgroundColor: "#FFEDF9",
+//     height: "7vw",
+//     duration: 0.2,
+//     scrollTrigger:{
+//         trigger: "#nav",
+//         scroller: "body",
+//         //markers: true,
+//         start: "top -60%",
+//         end: "top -11%",
+//         scrub: 1,
+//     }
+// })
+
+gsap.to("#main",{
+    backgroundColor: "#FDFDFD",
+    scrollTrigger:{
+        trigger: "#main",
+        scroller: "body",
+        //markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2, 
+    }
+})
+
+gsap.from("#page2 h1, #page2 img, #page2 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page3 h1, #page3 img, #page3 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page4 h1, #page4 img, #page4 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page4",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+var typed = new Typed('#element', {
+    strings: ["<i></i> What's Your Carbon Footprints? "],
+    typeSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 1500,
+    backSpeed: 50,
+  });
+
+  gsap.from("#page6 #element, #page6 img,#page6 p, #page6 btn",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page6",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page5 map,#page5 p, #page5 h1, #page5 h3",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page5",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+var typed = new Typed('#element2', {
+    strings: ["<i></i> Local Community  Gardens"],
+    typeSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 1500,
+    backSpeed: 50,
+  });
+
+  gsap.from("#page7 img,#page7 p,#page7 h3,#page7 i ",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page7",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page8 img,#page8 p,#page8 h3,#page8 i ",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page8",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page9 img,#page9 p,#page9 h3,#page9 i ",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page9",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+function openOverlay1() {
+    document.getElementById("overlay1").style.display = "flex";
+}
+  
+function openOverlay2() {
+    document.getElementById("overlay2").style.display = "flex";
+}
+  
+function openOverlay3() {
+    document.getElementById("overlay3").style.display = "flex";
+}
+
+function openOverlay4() {
+    document.getElementById("overlay4").style.display = "flex";
+}
+
+function closeOverlay1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+  
+function closeOverlay2() {
+    document.getElementById("overlay2").style.display = "none";
+}
+  
+function closeOverlay3() {
+    document.getElementById("overlay3").style.display = "none";
+}
+
+function closeOverlay4() {
+    document.getElementById("overlay4").style.display = "none";
+}
